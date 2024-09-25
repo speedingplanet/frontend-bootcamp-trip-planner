367be614b35117a9afad6d51f330ac1385e5f0db
John Paxton <johnpaxton@gmail.com>
2024-09-25 10:42:04 -0400

    Small horizontal margins

--- src/App.tsx
+++ src/App.tsx
@@ -10,91 +10,93 @@ const getClassesWhenActive = ({ isActive }: NavLinkRenderProps) =>
 
 function App() {
 	return (
-		<BrowserRouter>
-			{/* nav>ul>li>NavLink */}
-			<header className="mb-2 text-center text-2xl">
-				<nav>
-					<h1 style={{ fontFamily: 'National Park', fontWeight: '600' }}>
-						National Parks Social Trip Planner
-					</h1>
-					<hr />
-				</nav>
-			</header>
-			<nav className="my-2 text-center">
-				<NavLink
-					to="/"
-					className={({ isActive }) => (isActive ? 'font-bold underline' : 'hover:underline')}
-				>
-					Home
-				</NavLink>
-				&nbsp;|&nbsp;
-				<NavLink
-					to="/trips"
-					className={getClassesWhenActive}
-				>
-					Trips
-				</NavLink>
-				&nbsp;|&nbsp;
-				<NavLink
-					to="/locations"
-					className={({ isActive }) => {
-						if (isActive) {
-							return 'font-bold underline';
-						}
-						return 'hover:underline';
-					}}
-				>
-					Locations
-				</NavLink>
-				&nbsp;|&nbsp;
-				<NavLink
-					to="/friends"
-					className={({ isActive }) => {
-						if (isActive) {
-							return 'font-bold underline';
-						}
-						return 'hover:underline';
-					}}
-				>
-					Friends
-				</NavLink>
-				&nbsp;|&nbsp;
-				<NavLink
-					to="/settings"
-					className={({ isActive }) => {
-						if (isActive) {
-							return 'font-bold underline';
-						}
+		<main className="mx-4">
+			<BrowserRouter>
+				{/* nav>ul>li>NavLink */}
+				<header className="mb-2 text-center text-2xl">
+					<nav>
+						<h1 style={{ fontFamily: 'National Park', fontWeight: '600' }}>
+							National Parks Social Trip Planner
+						</h1>
+						<hr />
+					</nav>
+				</header>
+				<nav className="my-2 text-center">
+					<NavLink
+						to="/"
+						className={({ isActive }) => (isActive ? 'font-bold underline' : 'hover:underline')}
+					>
+						Home
+					</NavLink>
+					&nbsp;|&nbsp;
+					<NavLink
+						to="/trips"
+						className={getClassesWhenActive}
+					>
+						Trips
+					</NavLink>
+					&nbsp;|&nbsp;
+					<NavLink
+						to="/locations"
+						className={({ isActive }) => {
+							if (isActive) {
+								return 'font-bold underline';
+							}
+							return 'hover:underline';
+						}}
+					>
+						Locations
+					</NavLink>
+					&nbsp;|&nbsp;
+					<NavLink
+						to="/friends"
+						className={({ isActive }) => {
+							if (isActive) {
+								return 'font-bold underline';
+							}
+							return 'hover:underline';
+						}}
+					>
+						Friends
+					</NavLink>
+					&nbsp;|&nbsp;
+					<NavLink
+						to="/settings"
+						className={({ isActive }) => {
+							if (isActive) {
+								return 'font-bold underline';
+							}
 
-						return 'hover:underline';
-					}}
-				>
-					Settings
-				</NavLink>
-			</nav>
-			<Routes>
-				<Route
-					path="/"
-					element={<Home />}
-				/>
-				<Route
-					path="/trips"
-					element={<TripsManager />}
-				/>
-				<Route
-					path="/locations/*"
-					element={<LocationsManager />}
-				/>
-				<Route
-					path="/friends"
-					element={<FriendsManager />}
-				/>
-				<Route
-					path="/settings"
-					element={<SettingsManager />}
-				/>
-			</Routes>
-		</BrowserRouter>
+							return 'hover:underline';
+						}}
+					>
+						Settings
+					</NavLink>
+				</nav>
+				<Routes>
+					<Route
+						path="/"
+						element={<Home />}
+					/>
+					<Route
+						path="/trips"
+						element={<TripsManager />}
+					/>
+					<Route
+						path="/locations/*"
+						element={<LocationsManager />}
+					/>
+					<Route
+						path="/friends"
+						element={<FriendsManager />}
+					/>
+					<Route
+						path="/settings"
+						element={<SettingsManager />}
+					/>
+				</Routes>
+			</BrowserRouter>
+		</main>
 	);
 }
 
